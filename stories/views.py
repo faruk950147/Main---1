48131103d3dbe244d8ee7a9d9fe1bb4fc94bedfa
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-07-17 13:55:31 +0600

    Uploaded all files

--- stories/views.py
+++ stories/views.py
@@ -201,44 +201,43 @@ class GetColorsBySize(generic.View):
 class ReviewsView(LoginRequiredMixin, generic.View):
     login_url = reverse_lazy('sign')
     def post(self, request):  
-        if request.method == "POST":
-            try:
-                data = json.loads(request.body)
-                # Check if updating an existing review
-                review_id = data.get("review_id")  
-                # Get product ID from request
-                product_id = data.get("product_id")  
-                # Get the form data
-                subject = data.get("subject")
-                comment = data.get("comment")
-                rate = int(data.get("rate"))
+        try:
+            data = json.loads(request.body)
+            # Check if updating an existing review
+            review_id = data.get("review_id")  
+            # Get product ID from request
+            product_id = data.get("product_id")  
+            # Get the form data
+            subject = data.get("subject")
+            comment = data.get("comment")
+            rate = int(data.get("rate"))
+            
+            product = get_object_or_404(Product, id=product_id)  # Ensure product exists
                     
-                product = get_object_or_404(Product, id=product_id)  # Ensure product exists
-                    
-                # Rating validation (1 to 5)
-                if not (1 <= rate <= 5):
-                    return JsonResponse({"status": 400, "messages": "Invalid rating. Must be between 1 and 5."})
+            # Rating validation (1 to 5)
+            if not (1 <= rate <= 5):
+                return JsonResponse({"status": 400, "messages": "Invalid rating. Must be between 1 and 5."})
 
-                # Check if the user has already reviewed this product
-                if not review_id:
-                    existing_review = Review.objects.filter(product=product, user=request.user).first()
-                    if existing_review:
-                            return JsonResponse({"status": 400, "messages": "You have already reviewed this product."})
-                
-                if review_id:  # Editing an existing review
-                    review = get_object_or_404(Review, id=review_id, user_id=request.user.id)
-                    review.subject = subject
-                    review.comment = comment
-                    review.rate = rate
-                    review.save()
-                else:  # Creating a new review
-                    review = Review()
-                    review.product = product
-                    review.user_id = request.user.id
-                    review.subject = subject
-                    review.comment = comment
-                    review.rate = rate
-                    review.save()
+            # Check if the user has already reviewed this product
+            if not review_id:
+                existing_review = Review.objects.filter(product=product, user=request.user).first()
+                if existing_review:
+                    return JsonResponse({"status": 400, "messages": "You have already reviewed this product."})
+            
+            if review_id:  # Editing an existing review
+                review = get_object_or_404(Review, id=review_id, user_id=request.user.id)
+                review.subject = subject
+                review.comment = comment
+                review.rate = rate
+                review.save()
+            else:  # Creating a new review
+                review = Review()
+                review.product = product
+                review.user_id = request.user.id
+                review.subject = subject
+                review.comment = comment
+                review.rate = rate
+                review.save()
                 return JsonResponse({
                     "status": 200,
                     "review_id": review.id,
@@ -250,7 +249,7 @@ class ReviewsView(LoginRequiredMixin, generic.View):
                     "updated_date": review.updated_date.strftime('%Y-%m-%d %H:%M:%S'),
                     "messages": "Review added successfully"
                 })
-            except Review.DoesNotExist:
-                return JsonResponse({"status": 400, "messages": "Review not found for this user"})
-            except Exception as e:
-                return JsonResponse({"status": 400, "messages": str(e)})
+        except Review.DoesNotExist:
+            return JsonResponse({"status": 400, "messages": "Review not found for this user"})
+        except Exception as e:
+            return JsonResponse({"status": 400, "messages": str(e)})
